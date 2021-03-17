401ab29d36bdaa8a625ef050f9f162ed564ba5e7
David R <RosentalD16@chesterton.cambs.sch.uk>
2021-03-17 22:17:11 +0000

    Done random start and end point generation for both when maze is generated and when it isn't

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 
 if __name__ == '__main__':
-    visualizer.run(20, "A*", visualizer.RANDOM_MAZE)  \
+    visualizer.run(40, "A*", visualizer.RANDOM_START_AND_END)  \
         # side should be a drop down with all factors of 720, except 720, 360, 180, 240, 1, 2, 3, 4, 5
 
     sys.exit()
--- visualizer/generate.py
+++ visualizer/generate.py
@@ -1,4 +1,5 @@
 from .spot_stat import SpotState
+from random import randint  # for random_start_end
 
 
 def create_border(spot_grid, grid_side):
@@ -19,6 +20,30 @@ def random_maze(spot_grid, grid_side):
 
 def random_start_end(spot_grid, grid_side, maze_created):
     if maze_created:  # generate on borders, else anywhere
-        pass
+
+        y1 = 0  # to make y1 and y2 in scope
+        y2 = 0
+
+        invalid = True  # invalid until block on right is unvisited
+        while invalid:
+            y1 = randint(1, grid_side - 2)
+            if spot_grid[y1][1].state == SpotState.Unvisited:
+                invalid = False
+
+        invalid = True  # invalid until block on left is unvisited
+        while invalid:
+            y2 = randint(1, grid_side - 2)
+            if spot_grid[y2][grid_side - 2].state == SpotState.Unvisited:
+                invalid = False
+
+        spot_grid[y1][0].set_state(SpotState.Start)
+        spot_grid[y2][grid_side - 1].set_state(SpotState.End)
     else:
-        pass
+        x1 = randint(0, grid_side - 1)
+        y1 = randint(0, grid_side - 1)
+
+        x2 = randint(0, grid_side - 1)
+        y2 = randint(0, grid_side - 1)
+
+        spot_grid[y1][x1].set_state(SpotState.Start)
+        spot_grid[y2][x2].set_state(SpotState.End)
